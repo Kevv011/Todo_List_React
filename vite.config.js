a76608b8fd3c808ec3c69713313287e072a8004a
KevArevalo026 <kevinarepalacios123@gmail.com>
2025-01-11 19:12:56 -0600

    Sacando todo el proyecto a la raiz

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/KODIGO-Todo-List-firebase/Todo_List_React/',
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1000, 
